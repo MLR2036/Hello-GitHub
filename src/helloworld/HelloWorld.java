f9bea2963d3d191b743ce3224c319a0a37554aa9
unknown <mrodney@PC-CZC046BG0M.netbuilder.private>
2015-10-19 14:40:41 +0100

    first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+	int i = 0;
+	int j = 0;
+		while (i<5){
+			System.out.print("Hello World");
+			i++;
+			while(j<10){ 
+				System.out.print("Goodbye World");
+				j++;
+				i= 0;
+			}
+			j=0;
+			System.out.println();
+			
+	
+	}
+		
+	}
+}
